67f98ac66dff12c0af217add7728135515af228b
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-05-03 00:33:07 +0100

    change samthing in desgine admin

--- Front-End/src/pages/admin/Dashboard.jsx
+++ Front-End/src/pages/admin/Dashboard.jsx
@@ -153,12 +153,7 @@ export default function Dashboard({user, logout}) {
 
                   {menuOpen && (
                     <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-200 rounded-md shadow-lg z-20">
-                      <button
-                        onClick={() => navigate('/profile')}
-                        className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-gray-700"
-                      >
-                        Profil
-                      </button>
+                      
                       <button
                         onClick={handleLogout}
                         className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-gray-700"
